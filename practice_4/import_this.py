4fa64503f3882e80e2d2a30e3fa9299b7b553029
Ilya Lubenets <xfilinxcl@gmail.com>
2023-09-08 11:07:45 +0300

    practice 4 impoved

--- practice_4/import_this.py
+++ practice_4/import_this.py
@@ -1,5 +1,6 @@
 """Data for students for practice 4
 """
+import random
 from typing import TypeAlias
 
 FinishedPlace: TypeAlias = int
@@ -10,8 +11,30 @@ RacerTeam: TypeAlias = str
 RacerInfo: TypeAlias = dict[
     str, FinishedPlace | FinishedTimeSeconds | RacerName | RacerTeam
 ]
+RaceInfo: TypeAlias = dict[int, RacerInfo]
 
-RACE_DATA: dict[int, RacerInfo] = {
+"""
+Выиграл - VOVA!!! Поздравляем!!
+_______________________________
+
+Первые три места:
+
+Гонщик на 1 месте:
+    Имя: Vova
+    Команда: KAMAZ
+    Время: 01:00:00 (H:M:S)
+    
+Гонщик на 2 месте:
+    Имя: фыв
+    Команда: ыфв
+    Время: 02:00:00 (H:M:S)
+
+"""
+
+RANDOM_SEED = 1337
+random.seed(RANDOM_SEED)
+
+RACE_DATA: RaceInfo = {
     1: {
         'RacerName': 'Vova',
         'RacerTeam': 'KAMAZ',
@@ -43,3 +66,25 @@ RACE_DATA: dict[int, RacerInfo] = {
         'FinishedTimeSeconds': 4144,
     },
 }
+
+def generate_race_data(n_racers: int) -> RaceInfo:
+    teams = list(set(['KAMAZ', 'MAZ', 'PAZ', 'VAZ', 'TagAZ', 'ZAZ', 'GAZ']))
+    names = list(set(['Vova', 'Sergey', 'Andrey', 'Ilya', 'Elena', 'Galya', 'Sasha']))
+    places = list(range(1, n_racers + 1))
+    places = random.sample(places, len(places))
+    racers: list[RacerInfo] = [
+        {
+            'RacerName': random.choice(names),
+            'RacerTeam': random.choice(teams),
+            'FinishedPlace': places[-1],
+            'FinishedTimeSeconds': random.randint(1000 * places[-1], 1000 * places.pop() + 500),
+        } for i, _ in enumerate(range(1, n_racers + 1))
+    ]
+    
+    racers = random.sample(racers, len(racers))
+    
+    data: RaceInfo = {
+        ind: racer for ind, racer in enumerate(racers)
+    }
+    
+    return data
---
+++ practice_4/test.py
@@ -0,0 +1,7 @@
+from pprint import pprint
+
+from import_this import generate_race_data
+
+data = generate_race_data(10)
+
+pprint(data)
